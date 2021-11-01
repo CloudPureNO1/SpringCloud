9be94e9d3dd3756c5026c1dcbfaac9779575ad0f
CloudPureNO1 <CloudPure@163.com>
2021-11-01 14:21:45 +0800

    First

---
+++ spring-cloud-nacos/consumer/src/main/java/com/wxm/consumer/ConsumerApplication.java
@@ -0,0 +1,21 @@
+package com.wxm.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 在工程的启动文件开启FeignClient的功能：
+ * @EnableDiscoveryClient和@EnableEurekaClient共同点就是：都是能够让注册中心能够发现，扫描到改服务。
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-nacos/consumer/src/main/java/com/wxm/consumer/controller/ConsumerController.java
@@ -0,0 +1,31 @@
+package com.wxm.consumer.controller;
+
+import com.wxm.consumer.feign.ProviderFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>写一个contro 调用对应feign client  接口中的方法</p>
+ * <p></p>
+ *
+ * @author 王森明
+ * @date 2021/11/1 13:17
+ * @since 1.0.0
+ */
+@RequestMapping("consumer")
+@RestController
+public class ConsumerController {
+    @Value("${server.port}")
+    private String serverPort;
+    @Autowired
+    private ProviderFeignClient providerFeignClient;
+
+    @GetMapping("/showMsg")
+    private String getMsg(@RequestParam(value="name",required = false) String name){
+        return providerFeignClient.getMsg(name)+", The current access comes from a consumer with port number "+serverPort;
+    }
+}
